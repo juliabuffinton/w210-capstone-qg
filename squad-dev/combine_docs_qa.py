a65123c3adaff994f2aeb892a46911eb8e2ba384
Joanna Huang <joanna.huang@ischool.berkeley.edu>
2019-03-08 16:17:45 -0800

    code for combining docs and qa

---
+++ squad-dev/combine_docs_qa.py
@@ -0,0 +1,20 @@
+import json
+import ast
+
+with open('DrQA-input-squad-dev.json') as f:
+  docs_qs = json.load(f)
+
+ans_file = open('DrQA-input-squad-dev-single.preds','r')
+answers = ast.literal_eval(ans_file.read())
+
+docs_qa = {}
+for key, value in docs_qs.items():
+	qa = {}
+	for k, v in answers.items():
+		for q in range(len(value)):
+			if int(k) == int(value[q][‘qid’]):
+				qa[value[q][‘question’]] = v
+	docs_qa[key] = qa
+
+with open('doc_qa-squad-dev.txt','w') as file:
+  file.write(json.dumps(docs_qa))
