247c64898b2b9d250c393f68b64af8251ddf95f3
Julia Buffinton <juliab@ischool.berkeley.edu>
2019-04-14 20:56:31 -0400

    scripts for preprocess & sent select

---
+++ sentence_selection.sh
@@ -0,0 +1,4 @@
+python sentence_selection.py \
+--input-dir wikipedia_data/wikipedia_squad_single/ \
+--out-dir-labeled wikipedia_data/labeled_sentences_single/ \
+--out-dir-unlabeled wikipedia_data/unlabeled_sentences_single/
